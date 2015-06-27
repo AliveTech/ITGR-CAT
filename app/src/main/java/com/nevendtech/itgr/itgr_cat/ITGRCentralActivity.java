45d2212337b1401dea621ba61a301d45f31caf4a
AliveTech <torjeireime@gmail.com>
2015-06-27 22:52:19 +0200

    Started designing layout and programming buttons for changing activities

--- app/src/main/java/com/nevendtech/itgr/itgr_cat/ITGRCentralActivity.java
+++ app/src/main/java/com/nevendtech/itgr/itgr_cat/ITGRCentralActivity.java
@@ -4,22 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ITGRCentralActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_itgrcentral);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_itgrcentral, menu);
         return true;
     }
 
@@ -36,11 +34,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when the user click the 'IT Grossisten' button */
-    public void goToITGRCA(View view){
-        Intent intent = new Intent(this, ITGRCentralActivity.class);
-        startActivity(intent);
     }
 }
