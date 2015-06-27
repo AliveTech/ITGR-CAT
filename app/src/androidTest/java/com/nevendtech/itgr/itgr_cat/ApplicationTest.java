803951bda95efef38444bea4b859ccb25157867f
AliveTech <torjeireime@gmail.com>
2015-06-27 22:11:12 +0200

    Test commit

---
+++ app/src/androidTest/java/com/nevendtech/itgr/itgr_cat/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.nevendtech.itgr.itgr_cat;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
